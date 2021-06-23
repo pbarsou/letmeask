7b4ddb6cbdaa72174ca8ac8654f06133d736bb8a
Pablo Barbosa <pablo.barsou@gmail.com>
2021-06-22 21:58:09 -0300

    Integrando projeto ao Firebase

---
+++ src/services/firebase.ts
@@ -0,0 +1,18 @@
+import firebase from "firebase";
+import 'firebase/auth'; // importação do serviço de autenticação
+import 'firebase/database'; // importação do serviço de banco de dados
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const database = firebase.database();
